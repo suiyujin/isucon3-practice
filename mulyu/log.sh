8acab7a93f02ec0b510a2742f12823b73613f549
mulyu <mu0lyu@gmail.com>
2015-09-26 10:45:27 +0000

    update httpd

--- mulyu/log.sh
+++ mulyu/log.sh
@@ -1,2 +1,3 @@
-sudo isucon3 benchmark >> logs/bench_`date +%Y%m%d%H%m`
-sudo cat /etc/httpd/logs/access_log_wt | ./local/go/bin/bin/kataribe -f mulyu/kataribe.toml > logs/kataribe_log_`date +%Y%m%d%H%m`
+sudo isucon3 benchmark >> logs/bench_`date +%Y%m%d%H%M`
+sudo cat logs/access_log_wt | ./local/go/bin/bin/kataribe -f mulyu/kataribe.toml > logs/kataribe_log_`date +%Y%m%d%H%M`
+echo '' > logs/access_log_wt
